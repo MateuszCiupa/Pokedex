c00ca96278af74d362939d72e801b12ea3c37d3b
MateuszCiupa <01131212@pw.edu.pl>
2020-04-06 17:12:12 +0200

    Updates

---
+++ precache-manifest.cd6b63e69f5744ba568409011e497d33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69753f64cbab65e67ec98de0e5cc659c",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "af6959505c81edd2e900",
+    "url": "/pokedex/static/js/2.a0cd4087.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.a0cd4087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4121be4b1c3e55400deb",
+    "url": "/pokedex/static/js/main.a9fb1af5.chunk.js"
+  },
+  {
+    "revision": "6568b66a0bbf0e856034",
+    "url": "/pokedex/static/js/runtime-main.1e7056a3.js"
+  }
+]);
