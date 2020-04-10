b2d21f9b30c32c98ec1fab52bd912744a0062207
MateuszCiupa <01131212@pw.edu.pl>
2020-04-10 20:08:11 +0200

    simplify requests

--- src/util/pokemon.js
+++ src/util/pokemon.js
@@ -1,12 +1,11 @@
-export const pokeApiDefaultUrl = "https://pokeapi.co/api/v2/pokemon";
+export const POKE_LIMIT = 20;
+export const pokeUrl = "https://pokeapi.co/api/v2/pokemon";
+const url = "https://pokeapi.co/api/v2";
 
-export const fetchData = async (url) => {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    if (response.ok) return data;
-  } catch (e) {
-    console.log(e);
-  }
-  return null;
-};
+export const getPokemonCount = () => fetch(`${url}/pokemon?offset=0&limit=1`);
+
+export const getPokemons = () => fetch(`${url}/pokemon`);
+
+export const getPokemon = (n) => fetch(`${url}/pokemon/${n}`);
+
+export const getTypes = () => fetch(`${url}/type`);
