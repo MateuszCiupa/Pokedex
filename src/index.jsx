a8bb81bec856e28e934a37dc9bbd51b97132a392
MateuszCiupa <01131212@pw.edu.pl>
2020-04-10 20:10:15 +0200

    redux provider

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import theme from "util/theme";
 import { ThemeProvider } from "@material-ui/core/styles";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
