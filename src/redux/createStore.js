073fe84cb1196e3bc4c3f0b0e9fb1f824113f0f0
MateuszCiupa <01131212@pw.edu.pl>
2020-04-10 20:09:34 +0200

    just return store

--- src/redux/createStore.js
+++
@@ -1,6 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import reducer from "./modules";
-
-export default (preloadedState) =>
-  createStore(reducer, preloadedState, applyMiddleware(thunk));
