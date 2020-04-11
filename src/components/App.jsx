85655b55ef3057d9ae11ef41547b6b160d929e9f
MateuszCiupa <01131212@pw.edu.pl>
2020-04-11 22:33:30 +0200

    test next pokes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getPokeFirst, getPokeNext } from "redux/modules/pokemon";
 
-export default () => <div>yikes</div>;
+const App = ({ pokemon, loading, error, getPokeFirst, getPokeNext, stats }) => {
+  useEffect(() => {
+    getPokeFirst();
+  }, [getPokeFirst]);
+
+  useEffect(() => {
+    console.log(pokemon);
+  }, [pokemon]);
+
+  return (
+    <div>
+      {pokemon.results.map(({ name, weight, height }) => (
+        <div key={name}>
+          Name: {name}, weight: {weight}, height: {height}
+        </div>
+      ))}
+      <button disabled={loading} onClick={getPokeNext}>
+        Next
+      </button>
+    </div>
+  );
+};
+
+const mapStateToProps = ({ pokemon, loading, error, stats }) => ({
+  pokemon,
+  loading,
+  error,
+  stats,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getPokeFirst: () => dispatch(getPokeFirst()),
+  getPokeNext: () => dispatch(getPokeNext()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
