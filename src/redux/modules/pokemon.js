ef24912e8eca4765626970591f2d8b27535a8893
MateuszCiupa <01131212@pw.edu.pl>
2020-04-11 22:35:23 +0200

    poke first next

--- src/redux/modules/pokemon.js
+++ src/redux/modules/pokemon.js
@@ -1,64 +1,54 @@
-import shouldFilter from "util/filter";
-import { getPokeWithLimits, POKE_DISPLAY_LIMIT } from "util/pokemon";
+import { getPokeMax } from "util/pokemon";
 import { receiveError } from "./error";
 import { setPokeCount } from "./stats";
+import { receiveLoading } from "./loading";
 
-const RECEIVE_POKE_RESULT = "pokedex/pokemon/RECEIVE_POKE_RESULT";
+const RECEIVE_POKE_RESULTS = "pokedex/pokemon/RECEIVE_POKE_RESULTS";
 
-export const receivePokeResult = (payload) => ({
-  type: RECEIVE_POKE_RESULT,
+export const receivePokeResults = (payload) => ({
+  type: RECEIVE_POKE_RESULTS,
   payload,
 });
 
-const getPokeMax = () => async (dispatch) => {};
+export const getPokeFirst = () => async (dispatch, getState) => {
+  dispatch(receiveLoading(true));
+  dispatch(setPokeCount());
 
-export const getPokeFirst = () => async (dispatch, { filter, stats }) => {
-  try {
-    let response = await getPokeWithLimits(
-      0,
-      stats ? stats.pageLimit : POKE_DISPLAY_LIMIT
-    );
-    if (!response.ok) throw new Error(response.status);
-    const { count, results } = await response.json();
+  const { stats, filter } = getState();
+  const { pageLimit } = stats;
 
-    let limit = 0;
-    results.forEach(async ({ url }) => {
-      try {
-        response = await fetch(url);
-        if (!response.ok) throw new Error(response.status);
-        const {
-          name,
-          height,
-          weight,
-          types,
-          sprites,
-          base_experience,
-          abilities,
-        } = await response.json();
-        const pokemon = {
-          name,
-          height,
-          weight,
-          types,
-          sprites,
-          base_experience,
-          abilities,
-        };
-        if (!filter || !shouldFilter(pokemon, filter)) {
-          dispatch(receivePokeResult(pokemon));
-          limit++;
-        }
-      } catch (e) {
-        console.log(e);
-        dispatch(receiveError("Problem while getting poke details"));
-      }
-    });
+  let payload = await getPokeMax({
+    offset: 0,
+    filter,
+    pageLimit,
+  });
 
-    dispatch(setPokeCount(count));
-  } catch (e) {
-    console.log(e);
-    dispatch(receiveError("Problem while getting poke first"));
-  }
+  dispatch(receiveLoading(false));
+
+  if (!payload) return dispatch(receiveError("Problem with poke first"));
+
+  dispatch(receivePokeResults(payload));
+};
+
+export const getPokeNext = () => async (dispatch, getState) => {
+  dispatch(receiveLoading(true));
+
+  const { stats, filter, pokemon } = getState();
+  const { pageLimit, pokeCount } = stats;
+  const offset = pokemon.offset + pokemon.limit;
+
+  let payload = await getPokeMax({
+    offset,
+    filter,
+    pageLimit,
+    pokeCount,
+  });
+
+  dispatch(receiveLoading(false));
+
+  if (!payload) return dispatch(receiveError("Problem with poke next"));
+
+  dispatch(receivePokeResults({ ...payload, offset }));
 };
 
 const initialState = {
@@ -69,11 +59,10 @@ const initialState = {
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
-    case RECEIVE_POKE_RESULT:
+    case RECEIVE_POKE_RESULTS:
       return {
         ...state,
-        results: [...state.results, payload],
-        limit: state.limit + 1,
+        ...payload,
       };
 
     default:
