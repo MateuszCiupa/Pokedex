e630f1c8e6a365bac0b77ad3aac35a7a3131f6bd
MateuszCiupa <01131212@pw.edu.pl>
2020-04-11 13:54:39 +0200

    quality filter

---
+++ src/util/filter.js
@@ -0,0 +1,20 @@
+// cant guarantee that min or max is null or undefined (if max is null it means it's equal infinity)
+const inInterval = (x, { min, max }) =>
+  !!min ? (!!max ? x >= min && x <= max : x >= min) : !!max ? x <= max : true;
+
+// returns true if pokemon should be filtered, false - otherwise
+export default (pokemon, filter) => {
+  const { height: pheight, weight: pweight, types } = pokemon;
+  const { active, height: fheight, weight: fweight, type } = filter;
+
+  if (!active) return false;
+
+  if (
+    !inInterval(pheight, fheight) ||
+    !inInterval(pweight, fweight) ||
+    types.includes(type)
+  )
+    return true;
+
+  return false;
+};
