2b0ede31c5cad269c9e665ca005738558da20af3
MateuszCiupa <01131212@pw.edu.pl>
2020-04-12 20:38:20 +0200

    previous+next (filtered)

--- src/redux/modules/pokemon.js
+++ src/redux/modules/pokemon.js
@@ -1,55 +1,41 @@
-import { getPokeMax } from "util/pokemon";
+import { POKE_LIMIT_DEFAULT, getPokeWithLimits } from "util/pokemon";
 import { receiveError } from "./error";
-import { setPokeCount } from "./stats";
+import { setLeftLimit, setRightLimit, setPokeCount } from "./stats";
 import { receiveLoading } from "./loading";
+import { setFirstId } from "./filter";
+import shouldFilter from "util/filter";
 
 const RECEIVE_POKE_RESULTS = "pokedex/pokemon/RECEIVE_POKE_RESULTS";
+const RECEIVE_POKE_RESULT = "pokedex/pokemon/RECEIVE_POKE_RESULT";
+const RECEIVE_POKE_LIMIT = "pokedex/pokemon/RECEIVE_POKE_LIMIT";
+const RECEIVE_POKE_OFFSET = "pokedex/pokemon/RECEIVE_POKE_OFFSET";
+const RECEIVE_POKE_RESULT_BACKWARDS =
+  "pokedex/pokemon/RECEIVE_POKE_RESULT_BACKWARDS";
 
-export const receivePokeResults = (payload) => ({
+const receivePokeResults = (payload) => ({
   type: RECEIVE_POKE_RESULTS,
   payload,
 });
 
-export const getPokeFirst = () => async (dispatch, getState) => {
-  dispatch(receiveLoading(true));
-  dispatch(setPokeCount());
+const receivePokeResult = (payload) => ({
+  type: RECEIVE_POKE_RESULT,
+  payload,
+});
 
-  const { stats, filter } = getState();
-  const { pageLimit } = stats;
+const receivePokeLimit = (payload) => ({
+  type: RECEIVE_POKE_LIMIT,
+  payload,
+});
 
-  let payload = await getPokeMax({
-    offset: 0,
-    filter,
-    pageLimit,
-  });
+const receivePokeOffset = (payload) => ({
+  type: RECEIVE_POKE_OFFSET,
+  payload,
+});
 
-  dispatch(receiveLoading(false));
-
-  if (!payload) return dispatch(receiveError("Problem with poke first"));
-
-  dispatch(receivePokeResults(payload));
-};
-
-export const getPokeNext = () => async (dispatch, getState) => {
-  dispatch(receiveLoading(true));
-
-  const { stats, filter, pokemon } = getState();
-  const { pageLimit, pokeCount } = stats;
-  const offset = pokemon.offset + pokemon.limit;
-
-  let payload = await getPokeMax({
-    offset,
-    filter,
-    pageLimit,
-    pokeCount,
-  });
-
-  dispatch(receiveLoading(false));
-
-  if (!payload) return dispatch(receiveError("Problem with poke next"));
-
-  dispatch(receivePokeResults({ ...payload, offset }));
-};
+const receivePokeResultBackwards = (payload) => ({
+  type: RECEIVE_POKE_RESULT_BACKWARDS,
+  payload,
+});
 
 const initialState = {
   offset: 0,
@@ -62,10 +48,194 @@ export default (state = initialState, { type, payload }) => {
     case RECEIVE_POKE_RESULTS:
       return {
         ...state,
-        ...payload,
+        results: payload,
+      };
+
+    case RECEIVE_POKE_RESULT:
+      return {
+        ...state,
+        results: [...state.results, payload],
+      };
+
+    case RECEIVE_POKE_LIMIT:
+      return {
+        ...state,
+        limit: payload,
+      };
+
+    case RECEIVE_POKE_OFFSET:
+      return {
+        ...state,
+        offset: payload,
+      };
+
+    case RECEIVE_POKE_RESULT_BACKWARDS:
+      return {
+        ...state,
+        results: [payload, ...state.results],
       };
 
     default:
       return state;
   }
+};
+
+export const getPokeMax = () => async (dispatch, getState) => {
+  dispatch(receiveLoading(true));
+
+  const { filter, stats, pokemon } = getState();
+  if (!stats.pokeCount) dispatch(setPokeCount());
+  const { pokeCount = 964, pageLimit } = stats;
+  let { firstId } = filter;
+
+  let offset = pokemon.offset + pokemon.limit;
+
+  // TODO: store in buffor to load faster, currently it's dumping results
+  dispatch(receivePokeResults([]));
+  dispatch(receivePokeOffset(offset));
+
+  let limit = 0;
+  let received = 0;
+
+  try {
+    while (received < pageLimit && offset + limit < pokeCount) {
+      let response = await getPokeWithLimits(
+        offset + limit,
+        POKE_LIMIT_DEFAULT
+      );
+
+      if (!response.ok) throw new Error(response.status);
+
+      const { results } = await response.json();
+
+      for (let i = 0; i < results.length; i++) {
+        response = await fetch(results[i].url);
+        if (!response.ok) throw new Error(response.status);
+
+        // ikr, no other way tho
+        const {
+          name,
+          weight,
+          height,
+          abilities,
+          base_experience,
+          sprites,
+          types,
+          id,
+        } = await response.json();
+
+        const pokemon = {
+          name,
+          weight,
+          height,
+          abilities,
+          base_experience,
+          sprites,
+          types,
+          id,
+        };
+
+        limit++;
+
+        if (!filter.active || !shouldFilter(pokemon, filter)) {
+          dispatch(receivePokeResult(pokemon));
+          received++;
+          if (!firstId) {
+            firstId = id;
+            dispatch(setFirstId(id));
+          }
+          if (received === pageLimit) break;
+        }
+      }
+    }
+  } catch (e) {
+    console.log(e);
+    dispatch(receiveError("Problem while getting next pokes"));
+  }
+
+  dispatch(setLeftLimit(!(offset > 0)));
+  dispatch(setRightLimit(!(offset + limit < pokeCount)));
+  dispatch(receivePokeLimit(limit));
+  dispatch(receiveLoading(false));
+};
+
+export const getPokePrevious = () => async (dispatch, getState) => {
+  dispatch(receiveLoading(true));
+
+  const { filter, stats, pokemon } = getState();
+  const { pokeCount = 964, pageLimit } = stats;
+  const { firstId } = filter;
+
+  let offset = pokemon.offset;
+
+  if (offset === 0) return dispatch(receiveLoading(false));
+
+  dispatch(receivePokeResults([]));
+
+  let limit = 0;
+  let received = 0;
+
+  try {
+    while (received < pageLimit && offset - limit > 0) {
+      let response = await getPokeWithLimits(
+        offset - limit - POKE_LIMIT_DEFAULT < 0
+          ? 0
+          : offset - limit - POKE_LIMIT_DEFAULT,
+        offset - limit - POKE_LIMIT_DEFAULT < 0
+          ? offset - limit
+          : POKE_LIMIT_DEFAULT
+      );
+
+      if (!response.ok) throw new Error(response.status);
+
+      const { results } = await response.json();
+
+      for (let i = results.length - 1; i >= 0; i--) {
+        response = await fetch(results[i].url);
+        if (!response.ok) throw new Error(response.status);
+
+        // ikr, no other way tho
+        const {
+          name,
+          weight,
+          height,
+          abilities,
+          base_experience,
+          sprites,
+          types,
+          id,
+        } = await response.json();
+
+        const pokemon = {
+          name,
+          weight,
+          height,
+          abilities,
+          base_experience,
+          sprites,
+          types,
+          id,
+        };
+
+        limit++;
+
+        if (!filter.active || !shouldFilter(pokemon, filter)) {
+          dispatch(receivePokeResultBackwards(pokemon));
+          received++;
+          if (received === pageLimit) break;
+        }
+      }
+    }
+  } catch (e) {
+    console.log(e);
+    dispatch(receiveError("Problem while getting previous pokes"));
+  }
+
+  offset -= limit;
+  console.log(firstId);
+  dispatch(receivePokeOffset(offset));
+  dispatch(setLeftLimit(!(offset > 0) || offset + 1 === firstId));
+  dispatch(setRightLimit(!(offset + limit < pokeCount)));
+  dispatch(receivePokeLimit(limit));
+  dispatch(receiveLoading(false));
 };
