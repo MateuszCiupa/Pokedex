fb98f5e46c816fa789870e5ce7dd6d58de0eabad
MateuszCiupa <01131212@pw.edu.pl>
2020-04-14 11:59:06 +0200

    page change on buffer change

--- src/redux/modules/stats.js
+++ src/redux/modules/stats.js
@@ -1,28 +1,24 @@
-import { POKE_DISPLAY_LIMIT, pokeCountUrl } from "util/pokemon";
-import { receiveError } from "./error";
+import { POKE_DISPLAY_LIMIT } from "util/pokemon";
+import { RECEIVE_BUFFER_RESULT } from "./pokeBuffer";
 
 const SET_POKE_COUNT = "pokedex/stats/SET_POKE_COUNT";
-const SET_PAGE_LIMIT = "pokedex/stats/SET_PAGE_LIMIT";
 const INCR_PAGE = "pokedex/stats/INCR_PAGE";
 const DECR_PAGE = "pokedex/stats/DECR_PAGE";
 const SET_LEFT_LIMIT = "pokedex/stats/SET_LEFT_LIMIT";
 const SET_RIGHT_LIMIT = "pokedex/stats/SET_RIGHT_LIMIT";
+const SET_CURRENT_PAGE = "pokedex/stats/SET_CURRENT_PAGE";
+const SET_PAGE_COUNT = "pokedex/stats/SET_PAGE_COUNT";
 
 const initialState = {
   pokeCount: undefined,
-  currentPage: 0,
-  pageLimit: POKE_DISPLAY_LIMIT,
+  pageCount: 1,
+  currentPage: 1,
   leftLimit: true,
   rightLimit: false,
 };
 
-const _setPokeCount = (payload) => ({
+export const setPokeCount = (payload) => ({
   type: SET_POKE_COUNT,
-  payload,
-});
-
-export const setPageLimit = (payload) => ({
-  type: SET_PAGE_LIMIT,
   payload,
 });
 
@@ -44,39 +40,22 @@ export const setRightLimit = (payload) => ({
   payload,
 });
 
-export const updateLimits = () => (dispatch, getState) => {
-  const { pokemon, stats } = getState();
-  const { offset, limit } = pokemon;
-  const { pokeCount } = stats;
+export const setCurrentPage = (payload) => ({
+  type: SET_CURRENT_PAGE,
+  payload,
+});
 
-  dispatch(setLeftLimit(!(offset > 0)));
-  dispatch(setRightLimit(!(offset + limit < pokeCount)));
-};
+export const setPageCount = (payload) => ({
+  type: SET_PAGE_COUNT,
+  payload,
+});
 
-export const setPokeCount = () => async (dispatch) => {
-  try {
-    const response = await fetch(pokeCountUrl);
-    if (!response.ok) throw new Error(response.status);
-    const { count } = await response.json();
-    dispatch(_setPokeCount(count));
-  } catch (e) {
-    console.log(e);
-    dispatch(receiveError("Problem while getting poke count"));
-  }
-};
-
-export default (state = initialState, { type, payload }) => {
+export default (state = initialState, { type, payload, index }) => {
   switch (type) {
     case SET_POKE_COUNT:
       return {
         ...state,
         pokeCount: payload,
-      };
-
-    case SET_PAGE_LIMIT:
-      return {
-        ...state,
-        pageLimit: payload,
       };
 
     case INCR_PAGE:
@@ -102,6 +81,21 @@ export default (state = initialState, { type, payload }) => {
         ...state,
         leftLimit: !!payload,
       };
+
+    case SET_CURRENT_PAGE:
+      return {
+        ...state,
+        currentPage: payload,
+      };
+
+    case RECEIVE_BUFFER_RESULT:
+      const page = Math.ceil(index / POKE_DISPLAY_LIMIT);
+      if (state.pageCount < page)
+        return {
+          ...state,
+          pageCount: page,
+        };
+      return state;
 
     default:
       return state;
