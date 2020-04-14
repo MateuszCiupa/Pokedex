5a1a7a45c371c725fbc76fe5b764c175eb563639
MateuszCiupa <01131212@pw.edu.pl>
2020-04-14 12:04:17 +0200

    Updates

---
+++ precache-manifest.4314e8751d5508e3b3af4acd325abc26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5035d4eff32aafb18f85cbb1559df3",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "948074a9fe09c56f79f0",
+    "url": "/pokedex/static/js/2.f86a4533.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pokedex/static/js/2.f86a4533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24842669fad270e93fba",
+    "url": "/pokedex/static/js/main.3fd4ba33.chunk.js"
+  },
+  {
+    "revision": "6568b66a0bbf0e856034",
+    "url": "/pokedex/static/js/runtime-main.1e7056a3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pokedex/precache-manifest.223d7a26fb34606a5f4e7bc96dca98d3.js"
+  "/pokedex/precache-manifest.4314e8751d5508e3b3af4acd325abc26.js"
 );
 
 self.addEventListener('message', (event) => {
