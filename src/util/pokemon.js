a6848b3dc2e4f748def07e1081b0ce4af2c842e3
MateuszCiupa <01131212@pw.edu.pl>
2020-04-14 11:51:00 +0200

    get sprite url

--- src/util/pokemon.js
+++ src/util/pokemon.js
@@ -1,5 +1,4 @@
-export const POKE_LIMIT_DEFAULT = 20;
-export const POKE_DISPLAY_LIMIT = 20;
+export const POKE_DISPLAY_LIMIT = 24;
 export const apiUrl = "https://pokeapi.co/api/v2";
 export const pokeUrl = `${apiUrl}/pokemon`;
 export const pokeCountUrl = `${pokeUrl}?offset=0&limit=1`;
@@ -8,3 +7,22 @@ export const getTypes = () => fetch(`${apiUrl}/type`);
 
 export const getPokeWithLimits = (offset, limit) =>
   fetch(`${pokeUrl}?offset=${offset}&limit=${limit}`);
+
+export const getSprite = (sprites = {}) => {
+  const {
+    front_default,
+    front_shiny,
+    front_female,
+    front_shiny_female,
+  } = sprites;
+
+  return front_default
+    ? front_default
+    : front_shiny
+    ? front_shiny
+    : front_female
+    ? front_female
+    : front_shiny_female
+    ? front_shiny_female
+    : `${process.env.PUBLIC_URL}/img/PokeUnknown.png`;
+};
