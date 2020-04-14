87f6b51e09ea873548c66680e6f49244ceaa3d11
MateuszCiupa <01131212@pw.edu.pl>
2020-04-14 11:59:54 +0200

    pokeBuffer

--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -1,16 +1,16 @@
 import { combineReducers } from "redux";
-import pokemon from "./pokemon";
 import error from "./error";
 import loading from "./loading";
 import types from "./types";
 import filter from "./filter";
 import stats from "./stats";
+import pokeBuffer from "./pokeBuffer";
 
 export default combineReducers({
-  pokemon,
   error,
   loading,
   types,
   filter,
   stats,
+  pokeBuffer,
 });
