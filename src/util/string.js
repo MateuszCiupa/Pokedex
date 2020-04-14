ea67118d45de567b90847a218f63802e0f8fb6f5
MateuszCiupa <01131212@pw.edu.pl>
2020-04-14 11:49:24 +0200

    all to upper

--- src/util/string.js
+++ src/util/string.js
@@ -2,3 +2,6 @@ export const isString = (x) => typeof x === "string" && x.length > 0;
 
 export const firstToUpper = (x) =>
   isString(x) ? `${x[0].toUpperCase()}${x.slice(1, x.length)}` : "String";
+
+export const allToUpper = (x) =>
+  isString(x) ? x.split("-").map(firstToUpper).join(" ") : "String";
