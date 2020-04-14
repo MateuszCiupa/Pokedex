1fbb7915a163f76446b725024e7039ad3d3d22bd
MateuszCiupa <01131212@pw.edu.pl>
2020-04-14 19:28:40 +0200

    filter priority

--- src/components/PokeGrid/PokeGrid.jsx
+++ src/components/PokeGrid/PokeGrid.jsx
@@ -5,32 +5,33 @@ import PokeCard from "components/PokeCard";
 import { connect } from "react-redux";
 import { getSprite } from "util/pokemon";
 
-const PokeGrid = ({ pokeBuffer, stats: { currentPage } }) => {
+const PokeGrid = ({ pokeBuffer, stats: { currentPage }, filter }) => {
   const classes = useStyles();
   return (
     <div className={classes.container}>
       <Grid container spacing={3}>
-        {pokeBuffer.length >= currentPage &&
-          pokeBuffer[
-            currentPage - 1
-          ].map(({ name, weight, height, id, sprites }) => (
-            <PokeCard
-              key={name}
-              name={name}
-              imgUrl={getSprite(sprites)}
-              weight={weight}
-              height={height}
-              pokeId={id}
-            />
-          ))}
+        {(filter.active
+          ? filter.results[currentPage - 1]
+          : pokeBuffer[currentPage - 1]
+        ).map(({ name, weight, height, id, sprites }) => (
+          <PokeCard
+            key={name}
+            name={name}
+            imgUrl={getSprite(sprites)}
+            weight={weight}
+            height={height}
+            pokeId={id}
+          />
+        ))}
       </Grid>
     </div>
   );
 };
 
-const mapStateToProps = ({ pokeBuffer, stats }) => ({
+const mapStateToProps = ({ pokeBuffer, stats, filter }) => ({
   pokeBuffer,
   stats,
+  filter,
 });
 
 export default connect(mapStateToProps)(PokeGrid);
