735356f019a5d6dc9bded81fa4246274c28f2f95
MateuszCiupa <01131212@pw.edu.pl>
2020-04-14 19:24:39 +0200

    loading not needed

--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from "redux";
 import error from "./error";
-import loading from "./loading";
 import types from "./types";
 import filter from "./filter";
 import stats from "./stats";
@@ -8,7 +7,6 @@ import pokeBuffer from "./pokeBuffer";
 
 export default combineReducers({
   error,
-  loading,
   types,
   filter,
   stats,
